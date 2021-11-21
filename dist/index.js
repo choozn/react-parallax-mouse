28d310b926322cc92c8f128ea4759988c7d4757a
choozn <choozn@protonmail.com>
2021-11-21 11:16:12 +0100

    Build | Stable 1.3.1

--- dist/index.js
+++ dist/index.js
@@ -118,8 +118,10 @@ var MouseParallaxContainer = function (_a) {
                     // CSS Style Object
                     (_f = child.props.updateStyles, (_c = _f.transition, transition = _c === void 0 ? "" : _c, _d = _f.transform, transform = _d === void 0 ? "" : _d), rest = __rest(_f, ["transition", "transform"]));
             }
+            var transitionStyle = "" + ((!disableCSSTransition) && "transform 1e-7s linear") + ((transition) && ", ") + transition;
+            var transformStyle = "translateX(" + springOffset.x + "px) translateY(" + springOffset.y + "px) " + transform;
             // Apply Styles to each Child
-            return (React__default["default"].createElement("div", { className: (child.props.className) && child.props.className, style: __assign({ willChange: "transform", transition: "" + ((!disableCSSTransition) && "transform 1e-7s linear") + ((transition) && ", ") + transition, WebkitTransition: "" + ((!disableCSSTransition) && "transform 1e-7s linear") + ((transition) && ", ") + transition, msTransition: "" + ((!disableCSSTransition) && "transform 1e-7s linear") + ((transition) && ", ") + transition, transform: "translateX(" + springOffset.x + "px) translateY(" + springOffset.y + "px) " + transform, WebkitTransform: "translateX(" + springOffset.x + "px) translateY(" + springOffset.y + "px) " + transform, msTransform: "translateX(" + springOffset.x + "px) translateY(" + springOffset.y + "px) " + transform }, rest) }, child));
+            return (React__default["default"].createElement("div", { className: (child.props.className) && child.props.className, style: __assign({ willChange: "transform", transition: transitionStyle, WebkitTransition: transitionStyle, msTransition: transitionStyle, transform: transformStyle, WebkitTransform: transformStyle, msTransform: transformStyle }, rest) }, child));
         }))); }))));
 };
 
